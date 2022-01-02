fb155dbd60fc4d7af18cf8ffd190f698d5c85fa6
Juuro <mail@sebastian-engel.de>
2022-01-02 16:18:31 +0100

    Access environment variables.

--- netlify/functions/ghost.js
+++ netlify/functions/ghost.js
@@ -1,6 +1,6 @@
 exports.handler = async function(event, context) {
   return {
       statusCode: 200,
-      body: JSON.stringify({message: "Hello World"})
+      body: JSON.stringify({message: "Hello World", ghost_api: process.env.GHOST_API_URL})
   };
 }
