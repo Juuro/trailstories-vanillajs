55eeeddb8ce93183769c22df2758ea7dd9d1ef04
Juuro <mail@sebastian-engel.de>
2022-01-02 16:27:37 +0100

    Access Ghost API.

--- netlify/functions/ghost.js
+++ netlify/functions/ghost.js
@@ -1,6 +1,25 @@
 exports.handler = async function(event, context) {
+  const url = `${process.env.GHOST_API_URL}posts/?key=${process.env.GHOST_API_KEY}&include=tags`
+
+
+  if (event.queryStringParameters.slug) {
+
+  }
+
+  const options = {
+    method: "get",
+    headers: {
+      "Content-Type": "application/json"
+    }
+  }
+
+  const body = await fetch(url, options)
+    .then(response => {
+      return response.json()
+    })
+
   return {
       statusCode: 200,
-      body: JSON.stringify({message: "Hello World", event})
+      body: JSON.stringify({message: "Hello World", body})
   };
 }
