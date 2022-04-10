a024e605417b393a71acb03c136976b9fdcd2bc5
Sebastian Engel <sebastianengel@posteo.de>
2022-04-10 02:38:06 +0200

    chore: better custom element

--- static/js/components/Tag.js
+++ static/js/components/Tag.js
@@ -1,44 +1,41 @@
 export class Tag extends HTMLElement {
-  constructor() {
+  constructor() {    
     super()
 
-    const shadow = this.attachShadow({mode: 'open'})
-
-    const link = document.createElement('a')
-    const hyperlink = this.getAttribute('href')
-    link.setAttribute('href', hyperlink)
-    link.setAttribute('data-link', '')
-    const name = this.getAttribute('name')
-    link.textContent = name
-
-    const style = document.createElement('style')
-    style.textContent = `
+    this.template = document.createElement('template')
+    this.template.innerHTML = `
+      <style>
       :host {
-        font-size: 0.6rem;
-        opacity: 0.4;
-        transition: opacity 0.2s;
+        font-size: .6rem;
+        opacity: .4;
+        transition: opacity .2s;
       }  
       
       :host(:hover) {
         opacity: 1;
       }
-
-      a {
+    
+      :host a {
         font-size: 1em;
         background-color: var(--text-color);
-        padding: 0.1em 0.4em;
+        padding: .1em .4em;
         color: var(--pastel-red);
         text-decoration: none;
         text-transform: uppercase;
-        margin-right: 0.5em;
-        border-radius: 0.2em;
-        letter-spacing: 0.05em;
+        margin-right: .5em;
+        border-radius: .2em;
+        letter-spacing: .05em;
       }
-    `
+      </style>
 
-    shadow.appendChild(style)
-    shadow.appendChild(link)
+      <a href="${this.getAttribute('href')}" data-link="">${this.getAttribute('name')}</a>
+    `
+  }
+
+  connectedCallback() {
+    this.attachShadow({mode: 'open'})
+    this.shadowRoot.appendChild(this.template.content.cloneNode(true))
   }
 }
 
-customElements.define('tag-item', Tag);
+customElements.define('tag-item', Tag)
