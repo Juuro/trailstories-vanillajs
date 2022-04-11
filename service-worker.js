eb4d4189f08e0a625352bae8a8f2f47523a22d5c
Sebastian Engel <sebastianengel@posteo.de>
2022-04-11 01:03:26 +0000

    chore: delete abstract view

--- service-worker.js
+++ service-worker.js
@@ -10,7 +10,6 @@ const CACHED_URLS = [
   '/static/js/router.js',
   '/static/js/utilities.js',
   '/static/js/views/About.js',
-  '/static/js/views/AbstractView.js',
   '/static/js/views/Home.js',
   '/static/js/views/Imprint.js',
   '/static/js/views/Post.js',
